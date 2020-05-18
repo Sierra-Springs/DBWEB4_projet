590f1ec23e742b89c47a1dfbf94e76f4dd2a5641
Lefevre Nathanael <nathanael-1999@hotmail.fr>
2020-05-18 10:37:27 +0200

    push initial lien non ok

--- DB_connect.php
+++ DB_connect.php
@@ -1,10 +1,10 @@
 <?php
-	$servername = "192.168.1.46";
-	$username = "nathanael";
-	$password = "Zachaarone2010";
+	$servername = "pedago.univ-avignon.fr";
+	$username = "uapv1903416";
+	$password = "vr24nzm9";
 	
 	try{
-		$pdo = new PDO("pgsql:host=$servername; port=5433; dbname=test", $username, $password);
+		$pdo = new PDO("pgsql:host=$servername; dbname=test", $username, $password);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // mode d'erreur de PDO en "exception"
 // 		echo "Connected successfully"."<br>";
 	}
